9a8230aa1eb10f0ec5a5276ffc0f9a5a518fb0fe
Vitor656-ux <00001215173775sp@al.educacao.sp.gov.br>
2025-05-13 11:54:56 -0300

    Update exercicios05.py

--- exercicios05.py
+++ exercicios05.py
@@ -3,12 +3,14 @@ from exercicios04 import calcular_soma_e_media
 alunos = []
 
 def obter_dados_aluno():
-    nome_aluno = input('informe o seu nome completo')
-    serie_aluno =  input('informe sua serie')
-    email_aluno = input('informe o seu email')
-    nota01_aluno = input('informe sua nota')
-    nota01_aluno = input('informe sua nota')
-    nota01_aluno = input('informe sua nota')
+    nome_aluno = input('informe o seu nome completo: ')
+    serie_aluno =  input('informe sua serie: ')
+    email_aluno = input('informe o seu email: ')
+    nota01_aluno = int(input('informe a primeira nota do aluno: '))
+    nota02_aluno = int(input('informe a segunda nota do aluno: '))
+    nota03_aluno = int(input('informe a terceira nota do aluno: '))
+    
+    return cadastrar_aluno(nome_aluno, serie_aluno, email_aluno, nota01_aluno, nota02_aluno, nota02_aluno)
 
 
 def cadastrar_aluno(nome, serie, email, nota01=0, nota02=0, nota03=0):
@@ -27,8 +29,29 @@ def cadastrar_aluno(nome, serie, email, nota01=0, nota02=0, nota03=0):
 
     return alunos
 
-
 def mostrar_dados_alunos(dados_alunos):
-    return print(dados_alunos)
+    for aluno in dados_alunos:
+        print(f'nome do aluno: {aluno['nome do aluno']} email do aluno: {aluno['email']} | serie do aluno: {aluno['serie']} | media do aluno: {aluno['media']}')
+    
+    return
 
 mostrar_dados_alunos(alunos)
+
+def iniciar_sistema():
+    while True:
+        print('=' *80)
+        print('opcao 1 => mostrar lista de alunos cadastrados: ')
+        print('opcao 2 => cadastrar alunos: ')
+        print('opcao 3 => sair do sistema: ')
+        print('=' *80)
+
+        opcao = input('escolha uma das opções acima: ')
+
+        if opcao == '1':
+            mostrar_dados_alunos(alunos)
+        elif opcao == '2':
+            obter_dados_aluno
+        else:
+            print('sistema finalizado: ')
+
+iniciar_sistema()
